234ba51bf7a083be4846f3e5a019168754206101
Amarjit Pheiroijam <Amarjitp9@gmail.com>
2022-02-08 01:45:14 +0530

    Update Chat.jsx

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -80,10 +80,10 @@ function Chat({ socket, username, room }) {
                 </div>
               </div>
             } else {
-              return <div class="chat-message">
+              return <div class="chat-message mb-3">
                 <div class="flex items-end justify-end">
-                  <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                    <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">{messageContent.message}</span></div>
+                  <div class="flex flex-col text-xs max-w-xs mx-2 order-1 items-end">
+                    <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white">{messageContent.message}</span></div>
                     <p > {messageContent.author} at {messageContent.time}</p>
                   </div>
                   {/* <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-2" /> */}
